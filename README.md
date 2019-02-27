2fb5583fc9090f91e2ed0e421d8d33e597e8f6cd
Alexander Kolotov <alexandr.kolotov@gmail.com>
2019-02-27 17:27:01 +0300

    Initial commit from template [skip ci]

---
+++ README.md
@@ -0,0 +1,4 @@
+Assignement "Face Identification with Microsoft Face API"
+----
+
+At this moment only russian version of the assignement description is [available](README-ru.md).
---
+++ faceid.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import cognitive_face as cf
+from json import load
+import cv2
+
+BASE_URL = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/'
+
+### Put your code below this comment ###
