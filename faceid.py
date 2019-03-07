04e3d9699cad6662347d8fc8f289230df76854db
Yaroslav <yaroslav21savelev@gmail.com>
2019-03-07 08:48:12 +0300

    Test

--- faceid.py
+++ faceid.py
@@ -2,8 +2,3 @@
 
 import cognitive_face as cf
 from json import load
-import cv2
-
-BASE_URL = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/'
-
-### Put your code below this comment ###
